afb4fbf7cfeb3e14703b48968774772370762e29
terra <alterrael@yahoo.com>
2025-02-02 17:02:30 +0100

    Autocomplete setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { Autocomplete, TextField } from '@mui/material'
+const staedte = [ "Berlin", "Paris", "Madrid"];
+
+const addieren = (x : number, y: number) : number => {
+  return x + y;
+}
+// return typ ist implizit
+const addierenKurz = (x : number, y: number) => x+y;
+
+const helloWorld = () => console.log("hello world");
+
+const printArray = () => staedte.map(stadt => console.log(stadt))
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [stadt, setStadt] = useState<string | null>(null);
+  // useEffect(()=>{}, [data])
+  useEffect(
+    () => {console.log({stadt})}
+    , [stadt]);
 
   return (
     <>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+        <Autocomplete 
+        options={staedte}
+        value={stadt}
+        sx={{ width: 500 }}
+        onChange={(__event, value) => setStadt(value)}
+        renderInput={(params) => <TextField {...params} label="Bitte wähle eine Stadt aus" />}
+        />
+
+       </div>
     </>
   )
 }
